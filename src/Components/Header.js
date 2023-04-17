f47c1d246c1d3180117dd80dbb0913e08577b5b9
Shaun <scpour@gmail.com>
2023-04-17 10:55:09 -0400

    Navbar given base form

---
+++ src/Components/Header.js
@@ -0,0 +1,14 @@
+import './Header.css';
+
+const Header = () => {
+    return(
+        <ul className="nav">
+            <li><a href="">Home</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Location</a></li>
+            <li><a href="">Contact</a></li>
+        </ul>
+    )
+} 
+
+export default Header;
